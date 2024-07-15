bad79cca05a56b591bd1d400aff713466abf60b7
triduong <mtworkweb@gmail.com>
2024-07-15 23:38:23 +0700

    Config loading and embed User Logo Convex

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -7,6 +7,7 @@ import {
     Authenticated,
     ConvexReactClient
 } from "convex/react";
+import { Loading } from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
     children: React.ReactNode
@@ -24,7 +25,12 @@ export const ConvexClientProvider = ({
                 useAuth={useAuth}
                 client={convex}
             >
-                { children }
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <AuthLoading>
+                    <Loading />
+                </AuthLoading>
             </ConvexProviderWithClerk>
         </ClerkProvider>
     )
